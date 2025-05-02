bd8e3637b0aab79058fe95683c1122261320b219
vaidikdubey <vaidik26dubey@gmail.com>
2025-05-02 14:41:42 +0530

    fix: Updated menu button and slider styling. fix: Removed redundant code from reviews page script.

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,64 +1,18 @@
-//Loading screen functionality
-const preloader = document.getElementById("loading-screen");
+import { initLoadingScreen } from "./loadingScreen.js";
 
-window.addEventListener("load", () => {
-  preloader.classList.add("fade-out");
+initLoadingScreen();
 
-  setTimeout(() => {
-    preloader.style.display = "none";
-  }, 1000);
-});
 
-//Scroll to top arrow functionality
-const scrollToTop = document.querySelector(".scroll-to-top");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    scrollToTop.classList.remove("hidden");
-  } else {
-    scrollToTop.classList.add("hidden");
-  }
-});
-
-scrollToTop.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
-
-//Menu button functionality
-const menuButton = document.querySelector(".menu-button");
-const closeButton = document.querySelector(".close");
-const sliderMenu = document.querySelector(".slider");
-const mediaQuery = window.matchMedia("(min-width: 576px)");
-
-function closeMenu(size) {
-  if (size.matches) {
-    if (menuButton.classList.contains("active")) {
-      menuButton.classList.remove("active");
-      sliderMenu.classList.remove("active");
+window.addEventListener('load', async () => {
+    // Scroll to Top Arrow
+    if (document.querySelector(".scroll-to-top")) {
+    const { initScrollToTop } = await import("./scrollToTop.js");
+    initScrollToTop();
     }
-  }
-}
 
-closeMenu(mediaQuery);
-
-mediaQuery.addEventListener("change", closeMenu);
-
-menuButton.addEventListener("click", () => {
-  menuButton.classList.toggle("active");
-  sliderMenu.classList.toggle("active");
-});
-
-closeButton.addEventListener("click", () => {
-  menuButton.classList.toggle("active");
-  sliderMenu.classList.toggle("active");
-});
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    menuButton.classList.remove("active");
-    sliderMenu.classList.remove("active");
-  }
-});
+     // Menu functionality
+    if (document.querySelector(".menu-button")) {
+        const { initMenu } = await import("./menu.js");
+        initMenu();
+    }
+})
